5d1ff3afd9d4095a93c294122925fbfd39c603de
ryan <yuliantoryan64@gmail.com>
2024-03-04 23:10:06 +0700

    up

--- app/Http/Controllers/Admin/Dashboard/DashboardController.php
+++ app/Http/Controllers/Admin/Dashboard/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
     //
 
     public function index(){
-        $headerTitle = 'Dashboardd';
+        $headerTitle = 'Dashboard';
         return view('admin.dashboard.index', compact('headerTitle'));
     }
     
