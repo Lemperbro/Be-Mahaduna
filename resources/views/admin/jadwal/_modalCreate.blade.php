6aca922ba54809981dc24e48f67b046aaa8a649c
ryan <yuliantoryan64@gmail.com>
2024-06-08 13:10:34 +0700

    update build

--- resources/views/admin/jadwal/_modalCreate.blade.php
+++ resources/views/admin/jadwal/_modalCreate.blade.php
@@ -1,6 +1,6 @@
 <dialog id="createJadwal" class="modal modal-bottom sm:modal-middle">
     <div class="modal-box bg-white">
-        <form action="{{ route('jadwal.store') }}" method="POST">
+        <form id="jadwalForm" action="{{ route('jadwal.store') }}" method="POST">
             @csrf
             <h3 class="font-bold text-lg">Tambah Data Jadwal!</h3>
             <div class="mt-4">
@@ -51,20 +51,53 @@
                 noCalendar: true,
                 dateFormat: "H:i",
                 time_24hr: true,
-                theme: "light"
+                theme: "light",
+                onChange: function(selectedDates, dateStr, instance) {
+                    instance.input.setCustomValidity('');
+                }
             }
 
-            var endTime = flatpickr('#endTime', timepicker);
             var startTime = flatpickr('#startTime', timepicker);
+            var endTime = flatpickr('#endTime', timepicker);
+
+            $('#startTime').on('blur change', function() {
+                validateTimeInput(this);
+            });
+
+            $('#endTime').on('blur change', function() {
+                validateTimeInput(this);
+            });
+
+            function validateTimeInput(input) {
+                var regex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+                if (!regex.test(input.value)) {
+                    input.setCustomValidity('Format waktu tidak valid. Gunakan format HH:mm.');
+                } else {
+                    input.setCustomValidity('');
+                }
+                input.reportValidity();
+            }
 
             $('#createJadwalBtn').on('click', function() {
-                startTime.open();
-                endTime.open();
+                startTime.close();
+                endTime.close();
             });
 
             $('#closeModalCreateJadwal').on('click', function() {
                 const dialog = document.getElementById('createJadwal');
                 dialog.close();
+            });
+
+            $('#jadwalForm').on('submit', function(e) {
+                var startTimeInput = $('#startTime')[0];
+                var endTimeInput = $('#endTime')[0];
+
+                validateTimeInput(startTimeInput);
+                validateTimeInput(endTimeInput);
+
+                if (!startTimeInput.checkValidity() || !endTimeInput.checkValidity()) {
+                    e.preventDefault();
+                }
             });
         });
     </script>
